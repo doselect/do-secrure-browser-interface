b9e0f8eb238800d9d5eb7628539ef75f9c72722d
manish bharti <manish.bharti@infoedge.com>
2024-05-17 15:21:22 +0530

    notification logs..

--- src/components/ConfigureNotification/index.js
+++ src/components/ConfigureNotification/index.js
@@ -12,17 +12,52 @@ const ConfigureNotification = () => {
         cmd,
       };
       exec(CONFIGURE, payload, res => {
-        let filteredArr = res.result.split("\r");
-        // Remove all unwanted elements
-        console.log(filteredArr);
-        if (filteredArr.includes("0x0")) {
-          setNotificationInfo("Notifications are OFF");
-        } else if (filteredArr.includes("0x1")) {
-          setNotificationInfo("Notifications are ON");
-        } else {
-          setNotificationInfo("Notifications are ON");
+        if (res.error) {
+          setNotificationInfo(
+            "THERE'S SOMETHING ISSUE WITH RUNNING THE COMMAND"
+          );
+          // return;
         }
+        // let arr = res.result.split("\r");
+        const arr = [
+          "",
+          "\nHKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PushNotifications",
+          "\n    ToastEnabled    REG_DWORD    0x1",
+          "\n",
+          "\n",
+        ];
+
+        let toastEnabledValue = null;
+
+        // Iterate through the array to find the ToastEnabled value
+        for (let item of arr) {
+          if (item.includes("ToastEnabled")) {
+            // Split the item by spaces and get the last part which is the value
+            const parts = item.trim().split(/\s+/);
+            toastEnabledValue = parts[parts.length - 1];
+            break;
+          }
+        }
+
+        if (toastEnabledValue) {
+          console.log(`The value of ToastEnabled is ${toastEnabledValue}`);
+          if (toastEnabledValue === "0x0") {
+            console.log("Toast notifications are disabled.");
+          } else if (toastEnabledValue === "0x1") {
+            console.log("Toast notifications are enabled.");
+          } else {
+            console.log(
+              `The value of ToastEnabled is ${toastEnabledValue}, which is unexpected.`
+            );
+          }
+        } else {
+          console.log("The value of ToastEnabled could not be found.");
+        }
+
+        // Remove all unwanted elements
+
         console.log(res.result);
+        console.log(res);
       });
     }
   }, []);
