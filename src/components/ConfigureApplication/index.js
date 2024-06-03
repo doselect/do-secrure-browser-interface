4cdb7d7c23e02598df95cc94f8298f2efefe4cfe
manish bharti <manish.bharti@infoedge.com>
2024-06-03 12:08:06 +0530

    remove display apps.

--- src/components/ConfigureApplication/index.js
+++ src/components/ConfigureApplication/index.js
@@ -79,11 +79,11 @@ const ConfigureApplication = () => {
           </div>
         </span>
       </div>
-      <ul className="app-list">
+      {/* <ul className="app-list">
         {Array.from(runningProcess).map(process => (
           <li key={process}>{process}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
