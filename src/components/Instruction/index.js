080b2ea1bf72535cf9af02ce27c0f37b1fe9bd11
manish bharti <manish.bharti@infoedge.com>
2024-06-03 09:49:09 +0530

    add oader with hybrid approach.

--- src/components/Instruction/index.js
+++ src/components/Instruction/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
@@ -14,66 +14,108 @@ import {
   PROCEED_TEST_TEXT,
 } from "../../util/constant";
 import "./instruction.scss";
+const LoaderComponent = () => {
+  return (
+    <div className="loader-container">
+      <div className="spinner">
+        {[...Array(8)].map((_, index) => (
+          <div key={index} className={`dot dot-${index + 1}`}></div>
+        ))}
+      </div>
+      <p>Checking Device Compatibility</p>
+    </div>
+  );
+};
 
 const Instruction = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [deviceInfo, setDeviceInfo] = useState(null);
   const location = useLocation();
   // Extract query parameters from the location object
   const queryParams = new URLSearchParams(location.search);
   // Get the value of a specific query parameter
   const testUrl = queryParams.get("dsUrl");
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    let deviceType = "Unknown";
+    if (/Mobi|Android/i.test(userAgent)) {
+      deviceType = "Mobile";
+    } else {
+      deviceType = "Desktop";
+    }
+
+    let osType = "Unknown";
+    if (userAgent.indexOf("Win") !== -1) osType = "Windows";
+    if (userAgent.indexOf("Mac") !== -1) osType = "MacOS";
+    if (userAgent.indexOf("Linux") !== -1) osType = "Linux";
+    if (userAgent.indexOf("Android") !== -1) osType = "Android";
+    if (userAgent.indexOf("like Mac") !== -1) osType = "iOS";
+
+    if (deviceType !== "Desktop" || osType !== "Windows") {
+      window.location.href = testUrl;
+    }
+    setIsLoading(false);
+  }, []);
+
   return (
     <div className="instruction-container">
       <Header />
-      <div className="instruction-body">
-        <div className="instruction-header">
-          <img src={CandidateEnv} alt="candidateEnv" />
-          <div className="title-conatiner">
-            <p>Please Note:</p>
-            <p>
-              This assessment needs
-              <span className="link-text"> Doselect Secure Browser </span> to be
-              downloaded.
-            </p>
+      {isLoading ? (
+        <LoaderComponent />
+      ) : (
+        <div className="instruction-body">
+          <div className="instruction-header">
+            <img src={CandidateEnv} alt="candidateEnv" />
+            <div className="title-conatiner">
+              <p>Please Note:</p>
+              <p>
+                This assessment needs
+                <span className="link-text"> Doselect Secure Browser </span> to
+                be downloaded.
+              </p>
+            </div>
+          </div>
+          <div className="candidate-instruction">
+            <div className="title">
+              <img src={Warning} alt="Warning" />
+              <span className="instruction-text">{INSTRUCTION_TEXT}</span>
+              <ul>
+                {INSTRUCTION_TEXT_SUB_PARTS.map((instruction, index) => (
+                  <li key={index}>{instruction}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="horizontal-line" />
+          <div className="download-link-container">
+            <div className="title">{DOWNLOAD_LINK_TEXT}</div>
+            <div className="links-container">
+              <button
+                className="test-link primary"
+                onClick={() => {
+                  window.open(`Doselect://?dsUrl=${testUrl}`);
+                }}
+              >
+                {PROCEED_TEST_TEXT}
+                <img src={Proceed} alt="proceed" />
+              </button>
+              <button
+                className="download secondary"
+                onClick={() => {
+                  window.open(
+                    "https://dev-doselect-static.s3.ap-southeast-1.amazonaws.com/secure-browser/doselect_secure_browser_setup_1.0.1.exe"
+                  );
+                }}
+              >
+                {DOWNLOAD_BROWSER_TEXT}
+                <img src={FileDownload} alt="download" />
+              </button>
+            </div>
           </div>
         </div>
-        <div className="candidate-instruction">
-          <div className="title">
-            <img src={Warning} alt="Warning" />
-            <span className="instruction-text">{INSTRUCTION_TEXT}</span>
-            <ul>
-              {INSTRUCTION_TEXT_SUB_PARTS.map((instruction, index) => (
-                <li key={index}>{instruction}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="horizontal-line" />
-        <div className="download-link-container">
-          <div className="title">{DOWNLOAD_LINK_TEXT}</div>
-          <div className="links-container">
-            <button
-              className="test-link primary"
-              onClick={() => {
-                window.open(`Doselect://?dsUrl=${testUrl}`);
-              }}
-            >
-              {PROCEED_TEST_TEXT}
-              <img src={Proceed} alt="proceed" />
-            </button>
-            <button
-              className="download secondary"
-              onClick={() => {
-                window.open(
-                  "https://dev-doselect-static.s3.ap-southeast-1.amazonaws.com/secure-browser/doselect_secure_browser_setup_1.0.1.exe"
-                );
-              }}
-            >
-              {DOWNLOAD_BROWSER_TEXT}
-              <img src={FileDownload} alt="download" />
-            </button>
-          </div>
-        </div>
-      </div>
+      )}
       <Footer />
     </div>
   );
