3ea5758eb46c6ae1cf9bfc66c27289336a7c5bca
manish bharti <manish.bharti@infoedge.com>
2024-08-30 11:49:36 +0530

    non debian ubutu system changes....

---
+++ src/components/Instruction/Modal/SystemInfoModal.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import CandidateEnv from '../../../assets/icon/CandidateEnv.svg';
+import './systemInfoModal.scss';
+
+const SystemInfoModal = ({ setSubsytemInfo }) => {
+  const [selectedCheckbox, setSelectedCheckbox] = useState('');
+
+  const handleCheckboxChange = (event) => {
+    setSelectedCheckbox(event.target.id); // Update the state with the ID of the selected checkbox
+  };
+
+  const configureSubOs = () => {
+    setSubsytemInfo((prev) => ({
+      ...prev,
+      show: false,
+      info: selectedCheckbox,
+    }));
+  };
+  return (
+    <div className="system-info-modal-container">
+      <div>
+        <img className="candidate-env" src={CandidateEnv} alt="candidateEnv" />
+      </div>
+      <div className="info-container">
+        <div className="title">Please identify your device configuration :</div>
+        <div className="sub-title">
+          How can I check my device configuration?
+        </div>
+        <div className="action-container">
+          <label htmlFor="Debian based OS">
+            <input
+              id="Debian_based"
+              type="checkbox"
+              checked={selectedCheckbox === 'Debian_based'}
+              onChange={handleCheckboxChange}
+            />
+            My device is Linux DEB (Ubuntu)
+          </label>
+          <label htmlFor="Non Debian based OS">
+            <input
+              id="NonDebian_based"
+              type="checkbox"
+              checked={selectedCheckbox === 'NonDebian_based'}
+              onChange={handleCheckboxChange}
+            />
+            My device is Linux RPM (Red Hat, Fedora, SUSE)
+          </label>
+          <button
+            type="button"
+            // disabled={isCommandsInProgress}
+            className="configure-sub-os"
+            onClick={configureSubOs}
+          >
+            CONFIRM
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SystemInfoModal;
--- src/components/Instruction/index.js
+++ src/components/Instruction/index.js
@@ -23,9 +23,14 @@ import {
   initTracking,
   proctoringUBALogger,
 } from '../../util/trackingUtils';
+import SystemInfoModal from './Modal/SystemInfoModal';
 
 const Instruction = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [subsytemInfo, setSubsytemInfo] = useState({
+    show: false,
+    info: '',
+  });
   const location = useLocation();
   // Extract query parameters from the location object
   const queryParams = new URLSearchParams(location.search);
@@ -42,6 +47,12 @@ const Instruction = () => {
   }
 
   useEffect(() => {
+    if (osType === 'Linux') {
+      setSubsytemInfo((prev) => ({
+        ...prev,
+        show: true,
+      }));
+    }
     const ubaPayload = {
       pageName: 'Instruction Page',
     };
@@ -72,80 +83,96 @@ const Instruction = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (subsytemInfo.info === 'NonDebian_based') {
+      const decryptedUrl = decryptUrl(testUrl);
+      window.location.href = decryptedUrl;
+    }
+  }, [subsytemInfo]);
+
   return (
     <div className="instruction-container">
       <Header />
       {isLoading ? (
         <LoaderComponent title="" subText="Checking Device Compatibility" />
       ) : (
-        <div className="instruction-body">
-          <div className="instruction-header">
-            <img src={CandidateEnv} alt="candidateEnv" />
-            <div className="title-conatiner">
-              <p>Please Note:</p>
-              <p>
-                This assessment needs
-                <span className="link-text"> Doselect Secure Browser </span> to
-                be downloaded.
-              </p>
+        <>
+          {subsytemInfo?.show ? (
+            <SystemInfoModal setSubsytemInfo={setSubsytemInfo} />
+          ) : (
+            <div className="instruction-body">
+              <div className="instruction-header">
+                <img src={CandidateEnv} alt="candidateEnv" />
+                <div className="title-conatiner">
+                  <p>Please Note:</p>
+                  <p>
+                    This assessment needs
+                    <span className="link-text">
+                      {' '}
+                      Doselect Secure Browser{' '}
+                    </span>{' '}
+                    to be downloaded.
+                  </p>
+                </div>
+              </div>
+              <div className="candidate-instruction">
+                <div className="title">
+                  <img src={Warning} alt="Warning" />
+                  <span className="instruction-text">{INSTRUCTION_TEXT}</span>
+                  <ul>
+                    {INSTRUCTION_TEXT_SUB_PARTS.map((instruction, index) => (
+                      <li key={index}>{instruction}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div className="horizontal-line" />
+              <div className="download-link-container">
+                <div className="title">{DOWNLOAD_LINK_TEXT}</div>
+                <div className="links-container">
+                  <button
+                    type="button"
+                    className="test-link primary"
+                    onClick={() => {
+                      ctaClick({
+                        eventName: UBA_EVENT_NAME.proctoringTracker,
+                        payload: {
+                          label: candidateEmail,
+                          cta: 'Download secure browser',
+                          source: `Doselect://?email=${candidateEmail}&testName=${testName}&dsUrl=${testUrl}`,
+                        },
+                      });
+                      window.open(
+                        `Doselect://?email=${candidateEmail}&testName=${testName}&dsUrl=${testUrl}`
+                      );
+                    }}
+                  >
+                    {PROCEED_TEST_TEXT}
+                    <img src={Proceed} alt="proceed" />
+                  </button>
+                  <button
+                    type="button"
+                    className="download secondary"
+                    onClick={() => {
+                      ctaClick({
+                        eventName: UBA_EVENT_NAME.proctoringTracker,
+                        payload: {
+                          label: candidateEmail,
+                          cta: 'Download secure browser',
+                          source: getAppDownloadLink(osType),
+                        },
+                      });
+                      window.open(getAppDownloadLink(osType));
+                    }}
+                  >
+                    {DOWNLOAD_BROWSER_TEXT}
+                    <img src={FileDownload} alt="download" />
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="candidate-instruction">
-            <div className="title">
-              <img src={Warning} alt="Warning" />
-              <span className="instruction-text">{INSTRUCTION_TEXT}</span>
-              <ul>
-                {INSTRUCTION_TEXT_SUB_PARTS.map((instruction, index) => (
-                  <li key={index}>{instruction}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <div className="horizontal-line" />
-          <div className="download-link-container">
-            <div className="title">{DOWNLOAD_LINK_TEXT}</div>
-            <div className="links-container">
-              <button
-                type="button"
-                className="test-link primary"
-                onClick={() => {
-                  ctaClick({
-                    eventName: UBA_EVENT_NAME.proctoringTracker,
-                    payload: {
-                      label: candidateEmail,
-                      cta: 'Download secure browser',
-                      source: `Doselect://?email=${candidateEmail}&testName=${testName}&dsUrl=${testUrl}`,
-                    },
-                  });
-                  window.open(
-                    `Doselect://?email=${candidateEmail}&testName=${testName}&dsUrl=${testUrl}`
-                  );
-                }}
-              >
-                {PROCEED_TEST_TEXT}
-                <img src={Proceed} alt="proceed" />
-              </button>
-              <button
-                type="button"
-                className="download secondary"
-                onClick={() => {
-                  ctaClick({
-                    eventName: UBA_EVENT_NAME.proctoringTracker,
-                    payload: {
-                      label: candidateEmail,
-                      cta: 'Download secure browser',
-                      source: getAppDownloadLink(osType),
-                    },
-                  });
-                  window.open(getAppDownloadLink(osType));
-                }}
-              >
-                {DOWNLOAD_BROWSER_TEXT}
-                <img src={FileDownload} alt="download" />
-              </button>
-            </div>
-          </div>
-        </div>
+          )}
+        </>
       )}
       <Footer />
     </div>
